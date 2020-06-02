096db8029cc9c0c7165f950aae749cb0a291dbd4
denyo <denyo@users.noreply.github.com>
2020-06-02 09:59:04 +0000

    Automated deployment of example (master): 8969df7827cdb82523a7e5f7538b3e5e68078172

--- setup-test-env.js
+++
@@ -1,5 +0,0 @@
-import '@testing-library/jest-dom/extend-expect';
-
-jest.mock('@porsche-design-system/components-react', () => {
-    return require('@porsche-design-system/components-react/mocks');
-});
