4bd9b59af4dd8319c11c8d0af178725bb9be1986
Matthias Haegele <matthias.haegele@mhp.com>
2020-08-13 13:09:02 +0200

    fix: snapshot test mock site | mh/ bh | 769

--- src/components/__tests__/header.tsx
+++ src/components/__tests__/header.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-
 import Header from '../header';
+import * as Gatsby from 'gatsby';
+
+beforeEach(() => {
+  const useStaticQuery = jest.spyOn(Gatsby, 'useStaticQuery');
+  useStaticQuery.mockImplementation(() => ({
+    site: {
+      siteMetadata: {
+        author: 'Bernd / Matze',
+        description: 'Mock of SEO',
+        title: 'Porsche Design System in Gatsby'
+      },
+    },
+  }));
+});
 
 describe('Header', () => {
   it('renders correctly', () => {
